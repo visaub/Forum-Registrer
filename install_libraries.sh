741a8c4e72bc1c203e9091fe224c1969113aceed
VISAUB <v.sainz.ubide@gmail.com>
2019-02-17 22:41:25 -0600

    Changes to install_libraries.sh

--- install_libraries.sh
+++ install_libraries.sh
@@ -1,4 +1,25 @@
-git clone https://github.com/visaub/Forum-Registrer
+# git clone https://github.com/visaub/Forum-Registrer
 cd Forum-Registrer
-pip install Flask
-pip install requests
+
+install_list=(
+    'flask'
+    'flask_login'
+    'sqlite3'
+    'requests'
+    'time'
+    'unidecode'
+    'datetime'
+    'os'
+    'bs4'
+   )
+#
+# (Loop until we find an empty string.)
+#
+count=0
+while [ "x${install_list[count]}" != "x" ]
+do
+   count=$(( $count + 1 ))
+   echo -e 'Installing: '${install_list[count]}
+   pip3 install --user ${install_list[count]}
+done
+echo -e 'Libraries installed.'
